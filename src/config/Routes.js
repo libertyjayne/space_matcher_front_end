06b3b518c6b8b69d7003f8364b93d4d8d9958a37
Liberty <libertyjayne@gmail.com>
2021-10-10 08:45:48 -0500

    mui added

--- src/config/Routes.js
+++ src/config/Routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+import LoginView from '../pages/Login/LoginView';
 import AllSpacesPage from "../pages/AllSpaces";
 import NewSpacePage from "../pages/NewSpace";
 import FavoritesPage from "../pages/Favorites";
@@ -8,13 +9,16 @@ import FavoritesPage from "../pages/Favorites";
 function Routes() {
   return (
     <Switch>
-      <Route path="/" exact>
+      <Route path='/' exact>
         <AllSpacesPage />
       </Route>
-      <Route path="/new-space">
+      <Route path='/login'>
+        <LoginView />
+      </Route>
+      <Route path='/new-space'>
         <NewSpacePage />
       </Route>
-      <Route path="/favorites">
+      <Route path='/favorites'>
         <FavoritesPage />
       </Route>
     </Switch>
--- src/pages/Login/LoginView.js
+++ src/pages/Login/LoginView.js
@@ -1,5 +1,12 @@
 function LoginView() {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <h1>This is h1</h1>
+      <h2>This is h2</h2>
+      <h3>This is h3</h3>
+      <p>This is paragraph</p>
+    </div>
+  ); 
 }
 
 export default LoginView;
